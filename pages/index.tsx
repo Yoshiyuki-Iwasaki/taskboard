8194c6e452b69a82f2ecc922d906af514ce2ada7
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-08-25 15:46:57 +0900

    set firebase.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import TaskList from '../components/taskList'
+import TaskList from '../components/taskList';
+import Auth from "../components/Auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "../firebase/clientApp";
 
 export default function Home() {
-  return (
-    <>
-      <TaskList />
-    </>
-  );
+    const [user, loading, error] = useAuthState(firebase.auth());
+
+    if (loading) {
+      return <h6>Loading...</h6>;
+    }
+
+    if (error) {
+      return null;
+    }
+  return <>{!user ? <Auth /> : <TaskList />}</>;
 }
