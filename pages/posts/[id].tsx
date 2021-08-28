85c927a8cefb079c46f42362f4176bbdc8727d26
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-08-28 20:04:29 +0900

    create comment, people, tag function.

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -13,38 +13,115 @@ interface Comment {
 const index = ({ todo }) => {
   const db = firebase.firestore();
   const [user, loading, error] = useAuthState(firebase.auth());
-  const [text, setText] = useState("");
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [isChangedTodo, setIsChangedTodo] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [people, setPeople] = useState("");
+  const [tag, setTag] = useState("");
+  const [comment, setComment] = useState("");
+  // const [comments, setComments] = useState<Comment[]>([]);
   const convertJST = new Date();
   convertJST.setHours(convertJST.getHours());
   const updatedTime = convertJST.toLocaleString("ja-JP").slice(0, -3);
   const router = useRouter();
 
-  const [commentlists, commentlistsLoading, commentlistsError] = useCollection(
+  const [peoplelists, peoplelistsLoading, peoplelistsError] = useCollection(
     firebase
       .firestore()
-      .collection("comment")
+      .collection("people")
       .where("postID", "==", router.query.id)
   );
+  const [taglists, taglistsLoading, taglistsError] =
+    useCollection(
+      firebase
+        .firestore()
+        .collection("tag")
+        .where("postID", "==", router.query.id)
+  );
+  const [commentlists, commentlistsLoading, commentlistsError] =
+    useCollection(
+      firebase
+        .firestore()
+        .collection("comment")
+        .where("postID", "==", router.query.id)
+  );
 
-  const handleSubmit = async e => {
+  const handlePeopleSubmit = async e => {
     e.preventDefault();
-    if (!text) return;
-    setIsChangedTodo(true);
+    if (!people) return;
+    await db.collection("people").add({
+      id: new Date().getTime(),
+      text: people,
+      postID: router.query.id,
+      createdAt: updatedTime,
+    });
+    setPeople("");
+  };
+
+  const handleTagSubmit = async e => {
+    e.preventDefault();
+    if (!tag) return;
+    await db.collection("tag").add({
+      id: new Date().getTime(),
+      name: tag,
+      postID: router.query.id,
+      createdAt: updatedTime,
+    });
+    setTag("");
+  };
+
+  const handleCommentSubmit = async e => {
+    e.preventDefault();
+    if (!comment) return;
     await db.collection("comment").add({
       id: new Date().getTime(),
-      message: text,
+      message: comment,
       userId: user.uid,
       postID: router.query.id,
       createdAt: updatedTime,
     });
-    setText("");
+    setComment("");
   };
+
   return (
     <>
       <p>{todo.message}</p>
+      <h3>担当者</h3>
+      <ul>
+        {peoplelists &&
+          peoplelists.docs.map(doc => (
+            <li key={doc.data().id}>
+              <a href={`posts/${doc.data().id}`}>{doc.data().text}</a>
+            </li>
+          ))}
+      </ul>
+      <form onSubmit={e => handlePeopleSubmit(e)}>
+        <input
+          type="text"
+          value={people}
+          onChange={e => setPeople(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="追加"
+          onClick={e => handlePeopleSubmit(e)}
+        />
+      </form>
+      <h3>タグ</h3>
+      <ul>
+        {taglists &&
+          taglists.docs.map(doc => (
+            <li key={doc.data().id}>
+              <a href={`posts/${doc.data().id}`}>{doc.data().name}</a>
+            </li>
+          ))}
+      </ul>
+      <form onSubmit={e => handleTagSubmit(e)}>
+        <input
+          type="text"
+          value={tag}
+          onChange={e => setTag(e.target.value)}
+        />
+        <input type="submit" value="追加" onClick={e => handleTagSubmit(e)} />
+      </form>
+      <h3>コメント</h3>
       <ul>
         {commentlists &&
           commentlists.docs.map(doc => (
@@ -53,13 +130,17 @@ const index = ({ todo }) => {
             </li>
           ))}
       </ul>
-      <form onSubmit={e => handleSubmit(e)}>
+      <form onSubmit={e => handleCommentSubmit(e)}>
         <input
           type="text"
-          value={text}
-          onChange={e => setText(e.target.value)}
+          value={comment}
+          onChange={e => setComment(e.target.value)}
         />
-        <input type="submit" value="追加" onClick={e => handleSubmit(e)} />
+        <input
+          type="submit"
+          value="追加"
+          onClick={e => handleCommentSubmit(e)}
+        />
       </form>
     </>
   );
