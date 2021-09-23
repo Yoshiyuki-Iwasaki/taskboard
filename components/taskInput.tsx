a7df2393eaccb8fb4e9cf85da2f8253a60dbf205
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-09-23 19:38:17 +0900

    create taskinput component.

---
+++ components/taskInput.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const taskInput = ({ handleSubmit, text, setText }) => {
+  return (
+    <form onSubmit={e => handleSubmit(e)} className="mt-5	text-center">
+      <input
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+        className="border-4 border-light-blue-500 border-opacity-25 "
+      />
+      <input type="submit" value="追加" onClick={e => handleSubmit(e)} />
+    </form>
+  );
+};
+
+export default taskInput
