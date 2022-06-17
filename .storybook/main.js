9614572b3611711b4df9ffdd79f86c1e0df038dd
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-06-17 17:15:07 +0900

    🐛 remove storybook error.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,22 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  typescript: { reactDocgen: false },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config, { configType }) => {
+    // Fixes npm packages that depend on some modules
+    config.resolve = {
+      extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+      fallback: {
+        fs: false,
+        path: false
+      }
+    };
+
+    // Important: return the modified config
+    return config;
   }
-}
+};
